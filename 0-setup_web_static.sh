3ae29c6aeeda8e220da7fe4ec4f8b0876346e2ba
Ben Dosch <Benjamindosch@gmail.com>
2021-04-15 19:07:59 +0000

    Tasks added for Deploy static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static
+# Check if service exists
+service="nginx"
+if [ ! -f "/etc/init.d/$service" ]; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+# Check if directories exists
+DIRS=(
+    "/data"
+    "/data/web_static"
+    "/data/web_static/releases"
+    "/data/web_static/shared"
+    "/data/web_static/releases/test"
+)
+for DIR in ${DIRS[@]}; do
+    if [ ! -d "$DIR" ]; then
+        sudo mkdir -p "$DIR"
+    fi
+done
+# Check if file exists
+FILE="/data/web_static/releases/test/index.html"
+if [ ! -f "$FILE" ]; then
+    sudo touch "$FILE"
+fi
+# Add content to file
+content="<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
+echo "$content" | sudo tee /data/web_static/releases/test/index.html
+# Check if link exists and remove it
+LINK="/data/web_static/current"
+LINK_TARGET="/data/web_static/releases/test/"
+if [ -e "$LINK" ]; then
+    sudo rm "$LINK"
+fi
+# Create Link
+sudo ln -s "$LINK_TARGET" "$LINK"
+# Recusrively change owener and group to Ubuntu
+sudo chown -R ubuntu:ubuntu /data
+# Configure Nginx
+string="
+    server_name bendosch.tech;
+    location  hbnb_static {
+        alias /data/web_static/current/;
+        }
+"
+sudo sed -i "s/server_name _;/$string/" /etc/nginx/sites-enabled/default
---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that holds a fucntion for making a tgz archive of web_static"""
+import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """Function that makes a tgz archive of web_static"""
+    #create versions
+    local('if [ ! -d "versions" ]; then sudo mkdir "versions"; fi')
+    # add web_static fiels to tgx archive
+    time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    file = "versions/web_static_{}.tgz".format(time)
+    result = local("tar -czvf {} web_static".
+                   format(file))
+    #return arhive path if sucessful
+    if result.failed:
+        return None
+    return (file)
