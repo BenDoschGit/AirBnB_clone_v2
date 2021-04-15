6770d2b21d5446a5be50cb9627557988edf303ec
Ben Dosch <Benjamindosch@gmail.com>
2021-04-15 20:21:28 +0000

    sites enabled to sites avalible

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -52,5 +52,5 @@ sudo ln -s "$LINK_TARGET" "$LINK"
 # Recusrively change owener and group to Ubuntu
 sudo chown -R ubuntu:ubuntu /data
 # Configure Nginx
-sudo sed -i "s|server_name _;|server_name bendosch.tech;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t}\n|" /etc/nginx/sites-enabled/default
+sudo sed -i "s|server_name _;|server_name bendosch.tech;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t}\n|" /etc/nginx/sites-availavle/default
 sudo service nginx restart
