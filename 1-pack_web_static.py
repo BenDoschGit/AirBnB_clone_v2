1c80cabfa5a78d1b5fab3667050d4ec366184086
Ben Dosch <Benjamindosch@gmail.com>
2021-04-15 19:36:27 +0000

    took away line tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,14 +6,11 @@ from fabric.api import local
 
 def do_pack():
     """Function that makes a tgz archive of web_static"""
-    \\ create versions
     local('if [ ! -d "versions" ]; then sudo mkdir "versions"; fi')
-    \\ add web_static fiels to tgx archive
     time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     file = "versions/web_static_{}.tgz".format(time)
     result = local("tar -czvf {} web_static".
                    format(file))
-    \\ return arhive path if sucessful
     if result.failed:
         return None
     return (file)
