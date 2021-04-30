66effabcac45ca6d113d258810c6e1c9a175cccc
Ben Dosch <Benjamindosch@gmail.com>
2021-04-30 16:43:08 +0000

    taks 0 ready for checker

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for running the AirB&B clone project using flask"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """Sets up / to respond to HTTP get requests with hello HBNB"""
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
